8c0cd2840c01e14de269e36ef0d09331f4f1213c
Pedro Heliodoro <webmaster@ideiasfrescas.com>
2021-02-04 21:42:29 +0000

    adicionada traduçao pt-PT ao datatable

---
+++ Datatable/pt-PT.js
@@ -0,0 +1,22 @@
+const KdataTableTranslations = {
+    records: {
+        processing: "Aguarde por favor...",
+        noRecords: "Registos não encontrados"
+    },
+    toolbar: {
+        pagination: {
+            items: {
+                default: {
+                    first: "Primeiro",
+                    prev: "Anterior",
+                    next: "Seguinte",
+                    last: "Último",
+                    more: "Mais páginas",
+                    input: "Número de página",
+                    select: "Nº de registos por página"
+                },
+                info: "{{start}} - {{end}} de {{total}} registos"
+            }
+        }
+    }
+}
